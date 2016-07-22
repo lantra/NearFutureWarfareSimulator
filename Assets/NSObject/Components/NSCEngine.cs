0642746ee41e628e2d82c3bfd61d124e86663625
Lantra <lantra271@gmail.com>
2016-07-22 00:21:54 -0400

    Restructure, engine framework

---
+++ Assets/NSObject/Components/NSCEngine.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NSCEngine : NSComponent
+{
+    float topSpeed, Acceleration; //in kph
+    int maxNumberEngines, numberEngines;
+
+    public NSCEngine (string CN, float CHP, float CA, float tS, float acc, int Engines)
+    {
+        ComponentName = CN;
+        ComponentHP = CHP;
+        ComponentMaxHP = CHP;
+        ComponentArmor = CA;
+        topSpeed = tS;
+        Acceleration = acc;
+        maxNumberEngines = Engines;
+        numberEngines = Engines;
+    }
+
+    public float getSpeed()
+    {
+        if (ComponentMaxHP != ComponentHP)
+        {
+            topSpeed = topSpeed / (numberEngines/maxNumberEngines);
+        }
+        return topSpeed;
+    }
+
+}
